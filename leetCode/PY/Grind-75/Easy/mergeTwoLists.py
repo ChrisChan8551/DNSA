f63242edbd7578ebf8dd856d04353c2d516236b6
chrischan8551 <chrischan8551@gmail.com>
2024-05-23 13:42:45 -0500

    mergeTwoLists

--- leetCode/PY/Grind-75/Easy/mergeTwoLists.py
+++ leetCode/PY/Grind-75/Easy/mergeTwoLists.py
@@ -7,23 +7,20 @@
 # Return the head of the merged linked list.
 
 
-# Example 1:
-
-
+#! Example 1:
 # Input: list1 = [1,2,4], list2 = [1,3,4]
 # Output: [1,1,2,3,4,4]
-# Example 2:
 
+#! Example 2:
 # Input: list1 = [], list2 = []
 # Output: []
-# Example 3:
 
+#! Example 3:
 # Input: list1 = [], list2 = [0]
 # Output: [0]
 
 
 # Constraints:
-
 # The number of nodes in both lists is in the range [0, 50].
 # -100 <= Node.val <= 100
 # Both list1 and list2 are sorted in non-decreasing order.
@@ -34,25 +31,29 @@ class ListNode:
         self.next = None
 
 #! recursive
-
-
 def mergeTwoLists(list1, list2):
-    if (list1 == None):
-        return list2
-    if (list2 == None):
-        return list1
-    if (list1.val < list2.val):
-        next1 = list1.next
-        list1.next = mergeTwoLists(next1, list2)
-        return list1
-    else:
-        next2 = list2.next
-        list2.next = mergeTwoLists(list1, next2)
-        return list2
+    # use two queues, shift them onto a new list
+    # could modify one of the two lists
+    # start with lowest value between the two lists
+    # this will be the head returned
+    # recurse and look for next value
+    # list1.val < list2.val
+    # list1.next = mergeTwoLists(list1.next, list2)
+    def helper(list_1, list_2):
+        if (list1 == None):
+            return list2
+        if (list2 == None):
+            return list1
+        if (list1.val < list2.val):
+            next1 = list1.next
+            list1.next = mergeTwoLists(next1, list2)
+            return list1
+        else:
+            next2 = list2.next
+            list2.next = mergeTwoLists(list1, next2)
+            return list2
 
 #! iterative
-
-
 def mergeTwoLists(list1, list2):
     dummyHead = ListNode(0)
     tail = dummyHead
