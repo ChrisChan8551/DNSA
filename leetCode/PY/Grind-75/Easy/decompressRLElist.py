9b9b81c7ec5259f90b2157b2d07f80e604a80eb5
chrischan8551 <chrischan8551@gmail.com>
2024-05-24 16:48:16 -0500

    decompressRLElist

--- leetCode/PY/Grind-75/Easy/decompressRLElist.py
+++ leetCode/PY/Grind-75/Easy/decompressRLElist.py
@@ -34,6 +34,15 @@ def decompressRLElist(nums):
     return result
 
 
+def decompressRLElist(nums):
+    result = []
+    for i in range(0, len(nums), 2):
+        freq = nums[i]
+        val = nums[i + 1]
+        result.extend([val] * freq)
+    return result
+
+
 #! Example 1:
 nums = [1, 2, 3, 4]
 print(decompressRLElist(nums))
