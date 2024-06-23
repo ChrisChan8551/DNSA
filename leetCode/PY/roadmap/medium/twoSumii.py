e4fb6dec2c156718a9f8f50011ed1ba70444674a
ChrisChan8551 <chrischan8551@gmail.com>
2024-06-23 14:06:36 -0500

    twoSum

--- leetCode/PY/roadmap/medium/twoSumii.py
+++ leetCode/PY/roadmap/medium/twoSumii.py
@@ -9,18 +9,17 @@
 # Your solution must use only constant extra space.
 
 
-# Example 1:
-
+#! Example 1:
 # Input: numbers = [2,7,11,15], target = 9
 # Output: [1,2]
 # Explanation: The sum of 2 and 7 is 9. Therefore, index1 = 1, index2 = 2. We return [1, 2].
-# Example 2:
 
+#! Example 2:
 # Input: numbers = [2,3,4], target = 6
 # Output: [1,3]
 # Explanation: The sum of 2 and 4 is 6. Therefore index1 = 1, index2 = 3. We return [1, 3].
-# Example 3:
 
+#! Example 3:
 # Input: numbers = [-1,0], target = -1
 # Output: [1,2]
 # Explanation: The sum of -1 and 0 is -1. Therefore index1 = 1, index2 = 2. We return [1, 2].
@@ -34,5 +33,48 @@
 # -1000 <= target <= 1000
 # The tests are generated such that there is exactly one solution.
 
+#!brute force
+# def twoSum(numbers, target):
+#     n = len(numbers)
+#     for i in range(n):
+#         for j in range(n):
+#             if numbers[i] + numbers[j] == target:
+#                 return [i + 1, j + 1]
+
+
+#! binary solution
 def twoSum(numbers, target):
-    pass
+    # because numbers are given as a #! SORTED list
+    # you can use binary search as a solution here or 2 pointer
+    left, right = 0, len(numbers) - 1
+    while left < right:
+        current = numbers[left] + numbers[right]
+        if current == target:
+            return [left + 1, right + 1]
+        elif current < target:
+            left += 1
+        else:
+            right -= 1
+    return None
+
+
+#! Example 1:
+numbers = [2, 7, 11, 15]
+target = 9
+print(twoSum(numbers, target))
+# Output: [1,2]
+# Explanation: The sum of 2 and 7 is 9. Therefore, index1 = 1, index2 = 2. We return [1, 2].
+
+#! Example 2:
+numbers = [2, 3, 4]
+target = 6
+print(twoSum(numbers, target))
+# Output: [1,3]
+# Explanation: The sum of 2 and 4 is 6. Therefore index1 = 1, index2 = 3. We return [1, 3].
+
+#! Example 3:
+numbers = [-1, 0]
+target = -1
+print(twoSum(numbers, target))
+# Output: [1,2]
+# Explanation: The sum of -1 and 0 is -1. Therefore index1 = 1, index2 = 2. We return [1, 2].
