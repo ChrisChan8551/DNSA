c1de0933ff58d7c889df48c981df67958473e255
ChrisChan8551 <chrischan8551@gmail.com>
2024-09-04 23:12:46 -0500

    numberOfPairs

--- leetCode/PY/Grind-75/Easy/numberOfPairs.py
+++ leetCode/PY/Grind-75/Easy/numberOfPairs.py
@@ -16,12 +16,19 @@
 # Hint 1
 # The constraints are small. Check all pairs.
 
+#! brute force
 def numberOfPairs(nums1, nums2, k):
-    return
+    count = 0
+    for i in range(len(nums1)):
+        for j in range(len(nums2)):
+            if nums1[i] % (nums2[j] * k) == 0:
+                count += 1
+    return count
+
 
 #! Example 1:
-nums1 = [1,3,4]
-nums2 = [1,3,4]
+nums1 = [1, 3, 4]
+nums2 = [1, 3, 4]
 k = 1
 print(numberOfPairs(nums1, nums2, k))
 
@@ -32,8 +39,8 @@ print(numberOfPairs(nums1, nums2, k))
 # The 5 good pairs are (0, 0), (1, 0), (1, 1), (2, 0), and (2, 2).
 
 #! Example 2:
-nums1 = [1,2,4,12]
-nums2 = [2,4]
+nums1 = [1, 2, 4, 12]
+nums2 = [2, 4]
 k = 3
 print(numberOfPairs(nums1, nums2, k))
 
