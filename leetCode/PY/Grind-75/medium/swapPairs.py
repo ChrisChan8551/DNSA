385b0be47dfbe0e8d86c6a154c99bf55bb250d83
chrischan8551 <chrischan8551@gmail.com>
2024-09-26 13:15:27 -0500

    swapPairs

---
+++ leetCode/PY/Grind-75/medium/swapPairs.py
@@ -0,0 +1,34 @@
+# Given a linked list, swap every two adjacent nodes and return its head. You must solve the problem without modifying the values in the list's nodes (i.e., only nodes themselves may be changed.)
+
+
+
+def swapPairs(head):
+    return
+
+
+#! Example 1:
+head = [1, 2, 3, 4]
+print(swapPairs(head))
+# Output: [2,1,4,3]
+
+# Explanation:
+
+
+#! Example 2:
+head = []
+print(swapPairs(head))
+# Output: []
+
+#! Example 3:
+head = [1]
+print(swapPairs(head))
+# Output: [1]
+
+#! Example 4:
+head = [1, 2, 3]
+print(swapPairs(head))
+# Output: [2,1,3]
+
+# Constraints:
+# The number of nodes in the list is in the range [0, 100].
+# 0 <= Node.val <= 100
