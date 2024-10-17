82ecb79c80a8cc23c56392a9255c46f7366b8459
ChrisChan8551 <chrischan8551@gmail.com>
2024-10-17 14:34:59 -0500

    longestOnes

--- leetCode/PY/Grind-75/medium/longestOnes.py
+++ leetCode/PY/Grind-75/medium/longestOnes.py
@@ -18,11 +18,14 @@
 
 
 # Constraints:
-
 # 1 <= nums.length <= 105
 # nums[i] is either 0 or 1.
 # 0 <= k <= nums.length
+
 def longestOnes(nums, k):
+    # sliding window problem
+    # find max length of subarray where all digits can be 1's if we flip at most k number of zeroes
+    # extend window until we find max length or breaks the condition
     left = 0
     for right in range(len(nums)):
         if nums[right] == 0:
@@ -32,6 +35,25 @@ def longestOnes(nums, k):
                 k += 1
             left += 1
     return right - left + 1
+
+
+def longestOnes(nums, k):
+    left = 0
+    max_length = 0
+    zeroes = 0
+    for right, n in enumerate(nums):
+        if n == 0:
+            zeroes += 1
+
+        while zeroes > k:
+            if nums[left] == 0:
+                zeroes -= 1
+            left += 1
+
+        sub_length = right - left + 1
+        max_length = max(max_length, sub_length)
+
+    return max_length
 
 
 #! Example 1:
